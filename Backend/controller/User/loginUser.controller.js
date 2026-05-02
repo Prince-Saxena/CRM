306fb4b131f56d3fb2119ffa20b717da00ef7f24
Prince Saxena <princesaxena202020@gmail.com>
2026-05-02 09:10:41 +0530

    fix production cookies

--- Backend/controller/User/loginUser.controller.js
+++ Backend/controller/User/loginUser.controller.js
@@ -23,9 +23,9 @@ const loginUser = async (req, res) => {
 			const token = generateToken(user._id, user.role, user.name);
 			res.cookie("token", token, {
 				httpOnly: true,
-				secure: false, // true in production (HTTPS)
-				maxAge: 7 * 24 * 60 * 60 * 1000, // 7 day
-				sameSite: "lax",
+				secure: true,
+				sameSite: "none",
+				maxAge: 7 * 24 * 60 * 60 * 1000,
 			});
 			return res.status(200).json({
 				token,
